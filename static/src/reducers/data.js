6cc0636199213aba46b55d260c27b47983ad46b3
Alex Makumbi <alexmakumbi15@gmail.com>
2018-02-26 21:47:18 -0500

    added functions fetching JSON payload

--- static/src/reducers/data.js
+++ static/src/reducers/data.js
@@ -1,21 +1,132 @@
-import { RECEIVE_PROTECTED_DATA, FETCH_PROTECTED_DATA_REQUEST } from '../constants';
-import { createReducer } from '../utils/misc';
+import { FETCH_PROTECTED_DATA_REQUEST, RECEIVE_PROTECTED_DATA } from '../constants/index';
+import { parseJSON } from '../utils/misc';
+// Include required functions defined in http_functions.js fle
+import { data_about_user, nba_2016_2017_Season, competitions, premier_league, manchester_united, manchester_united_players, manchester_united_home_fixtures } from '../utils/http_functions';
+import { logoutAndRedirect } from './auth';
 
-const initialState = {
-    data: null,
-    isFetching: false,
-    loaded: false,
-};
+export function receiveProtectedData(data) {
+    return {
+        type: RECEIVE_PROTECTED_DATA,
+        payload: {
+            data,
+        },
+    };
+}
 
-export default createReducer(initialState, {
-    [RECEIVE_PROTECTED_DATA]: (state, payload) =>
-        Object.assign({}, state, {
-            data: payload.data,
-            isFetching: false,
-            loaded: true,
-        }),
-    [FETCH_PROTECTED_DATA_REQUEST]: (state) =>
-        Object.assign({}, state, {
-            isFetching: true,
-        }),
-});
+export function fetchProtectedDataRequest() {
+    return {
+        type: FETCH_PROTECTED_DATA_REQUEST,
+    };
+}
+
+export function fetchProtectedData(token) {
+    return (dispatch) => {
+        dispatch(fetchProtectedDataRequest());
+        data_about_user(token)
+            .then(parseJSON)
+            .then(response => {
+                dispatch(receiveProtectedData(response.result));
+            })
+            .catch(error => {
+                if (error.status === 401) {
+                    dispatch(logoutAndRedirect(error));
+                }
+            });
+    };
+}
+
+// fetch nba 2016-2017 season data
+export function fetch_NBA_2016_2017_Season_Data() {
+    return (dispatch) => {
+        nba_2016_2017_Season().then(parseJSON).then(response => {
+                dispatch(response.result);
+            });
+            // We are not catching errors for now
+            /*.catch(error => {
+                if (error.status === 401) {
+                    // *** will have to create proper error message***
+                    dispatch(logoutAndRedirect(error));
+                }
+            }); */
+    };
+}
+
+// fetch competition data
+export function fetchCompetitions() {
+    return (dispatch) => {
+        competitions().then(parseJSON).then(response => {
+                dispatch(response.result);
+            });
+            // We are not catching errors for now
+            /*.catch(error => {
+                if (error.status === 401) {
+                    // *** will have to create proper error message***
+                    dispatch(logoutAndRedirect(error));
+                }
+            }); */
+    };
+}
+
+// fetch premier_league data
+export function fetchPremierLeague() {
+    return (dispatch) => {
+        premier_league().then(parseJSON).then(response => {
+                dispatch(response.result);
+            });
+            // We are not catching errors for now
+            /*.catch(error => {
+                if (error.status === 401) {
+                    // *** will have to create proper error message***
+                    dispatch(logoutAndRedirect(error));
+                }
+            }); */
+    };
+}
+
+// fetch manchester_united data
+export function fetchManchesterUnited() {
+    return (dispatch) => {
+        manchester_united().then(parseJSON).then(response => {
+                dispatch(response.result);
+            });
+            // We are not catching errors for now
+            /*.catch(error => {
+                if (error.status === 401) {
+                    // *** will have to create proper error message***
+                    dispatch(logoutAndRedirect(error));
+                }
+            }); */
+    };
+}
+
+// fetch manchester_united_players data
+export function fetchManchesterUnitedPlayers() {
+    return (dispatch) => {
+        manchester_united_players().then(parseJSON).then(response => {
+                dispatch(response.result);
+            });
+            // We are not catching errors for now
+            /*.catch(error => {
+                if (error.status === 401) {
+                    // *** will have to create proper error message***
+                    dispatch(logoutAndRedirect(error));
+                }
+            }); */
+    };
+}
+
+// fetch manchester_united_home_fixtures data
+export function fetchManchesterUnitedHomeFixtures() {
+    return (dispatch) => {
+        manchester_united_home_fixtures().then(parseJSON).then(response => {
+                dispatch(response.result);
+            });
+            // We are not catching errors for now
+            /*.catch(error => {
+                if (error.status === 401) {
+                    // *** will have to create proper error message***
+                    dispatch(logoutAndRedirect(error));
+                }
+            }); */
+    };
+}
